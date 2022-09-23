9127d24a6039ab2114c8352fab69e46f42350d1f
Holyolg <63580448+Holyolg@users.noreply.github.com>
2022-09-23 21:49:42 +0300

    ok

---
+++ app.js
@@ -0,0 +1,13 @@
+const slides = document.querySelectorAll(".slide");
+
+slides.forEach((slide) => {
+  slide.addEventListener("click", function () {
+    removeActive();
+    slide.classList.add("active");
+  });
+});
+function removeActive() {
+  slides.forEach((slide) => {
+    slide.classList.remove("active");
+  });
+}
